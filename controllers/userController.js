4fcab685f7e7ddfbc5a0a3f8d1335096e11aa843
kezdetphia <fehermark88@gmail.com>
2023-10-24 16:22:55 -0700

    add jwt sign to controller func

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,28 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// Helper functions
+const createToken = (_id)=>{
+  // sign arguments: payload(_id),secret,options
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '2d'})
+}
+
+// Client functions
 
 // signup user
 const signupUser = async (req,res) => {
   const {email, password} = req.body
 
   try{
+    // get validated user back from Schema.statics func
     const user = await User.signup(email, password);
-    res.status(200).json({email, user})
+
+    // create token
+    // return signed jwt token from helper func above
+    const token = createToken(user._id)
+
+    //send data to client with email and signed token
+    res.status(200).json({email, token})
 
   }catch(error){
     res.status(400).json({error: error.message})
--- models/userModel.js
+++ models/userModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const bcrypt = require("bcrypt");
 const validator = require('validator')
 
-const Schema = mongoose.Schema
 
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   email: {
@@ -14,17 +14,15 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true,
-
   }
 }, {
   timestamps:true,
 }
 )
 
-
-// Mongoose model statics is a custom function that can be called just like create etc...
-//In this snipept it secures the password
-// Applies to the whole model rather than a specific instance
+// Static signup method
+// -Mongoose model statics is a custom function that can be called just like create etc...
+// -Applies to the whole model rather than a specific instance
 userSchema.statics.signup = async function(email, password){
 
   // validaton(using validator package)
@@ -32,13 +30,13 @@ userSchema.statics.signup = async function(email, password){
   if (!validator.isEmail(email)) throw Error('Invalid email')
   if (!validator.isStrongPassword(password)) throw Error('Password not strong enough')
 
-
+  // check database for existing email
   const emailExists = await this.findOne({email})
-
   if (emailExists) throw Error('Email is already registered')
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
+  //Create the user in the database 
   const user = await this.create({email, password: hash})
 
   return user
