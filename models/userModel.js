99f2ad72c3e25d6443fe97f38bc1fd71df183878
kezdetphia <fehermark88@gmail.com>
2023-10-24 15:24:45 -0700

    validating email and password with validator package

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -24,13 +25,20 @@ const userSchema = new Schema({
 // Mongoose model statics is a custom function that can be called just like create etc...
 //In this snipept it secures the password
 // Applies to the whole model rather than a specific instance
-userSchema.statics.signup = async function({email, password}){
-  const emailExists = this.findOne(email)
+userSchema.statics.signup = async function(email, password){
+
+  // validaton(using validator package)
+  if (!email || !password) throw Error('All fields must be filled');
+  if (!validator.isEmail(email)) throw Error('Invalid email')
+  if (!validator.isStrongPassword(password)) throw Error('Password not strong enough')
+
+
+  const emailExists = await this.findOne({email})
 
   if (emailExists) throw Error('Email is already registered')
 
-  const salt = await bcrypt.genSalt(10)
-  const hash = await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
   const user = await this.create({email, password: hash})
 
   return user
